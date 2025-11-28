41efe8528c54632e506d02df56fc15f93b5ef04f
albertmarcast-cloud <albert.mar.cast@gmail.com>
2025-11-27 23:10:46 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.href = 'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }, []);
+
   return <Component {...pageProps} />;
 }
