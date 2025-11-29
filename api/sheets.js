e55ba51e0b6c5c28fbbba2eed7e206fdbc869735
albertmarcast-cloud <albert.mar.cast@gmail.com>
2025-11-29 10:43:57 -0600

    Update sheets.js

--- api/sheets.js
+++ api/sheets.js
@@ -13,8 +13,10 @@ export default async function handler(req, res) {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(req.body)
-    });
+      body: JSON.stringify({
+  route: 'crearPedido',
+  ...req.body
+})
 
     const data = await response.json();
     
